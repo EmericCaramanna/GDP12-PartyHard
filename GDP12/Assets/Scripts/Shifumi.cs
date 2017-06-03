e7a9df529bf33512c014c36873b9ea50e7cbba9d
arnaud jomard <arnaud.jomard@epitech.eu>
2017-06-03 21:40:09 +0200

    Shifumi op MDR / start of Dance

--- GDP12/Assets/Scripts/Shifumi.cs
+++ GDP12/Assets/Scripts/Shifumi.cs
@@ -4,13 +4,80 @@ using UnityEngine;
 
 public class Shifumi : MonoBehaviour {
 
+    enum Symboru
+    {
+        pierre,
+        feuille,
+        ciseaux
+    }
+
+    enum resultGame
+    {
+        win,
+        lose,
+        again
+    }
+
+    bool play;
+    public int _signPlayer;
+    resultGame ah;
+    
+    void OnEnable ()
+    {
+        play = false;
+        _signPlayer = -1;
+    }
+
 	// Use this for initialization
 	void Start () {
-        
+        play = false;
+        _signPlayer = -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (!play && _signPlayer != -1)
+        {
+            play = true;
+            int signIA = (int)Random.Range(0f, 2.0f);
+            if (_signPlayer == (int)Symboru.pierre)
+            {
+                if (signIA == (int)Symboru.pierre)
+                    ah = resultGame.again;
+                else if (signIA == (int)Symboru.feuille)
+                    ah = resultGame.lose;
+                else
+                    ah = resultGame.win;
+            }
+            else if (_signPlayer == (int)Symboru.feuille)
+            {
+                if (signIA == (int)Symboru.pierre)
+                    ah = resultGame.win;
+                else if (signIA == (int)Symboru.feuille)
+                    ah = resultGame.again;
+                else
+                    ah = resultGame.lose;
+            }
+            else if (_signPlayer == (int)Symboru.ciseaux)
+            {
+                if (signIA == (int)Symboru.pierre)
+                    ah = resultGame.lose;
+                else if (signIA == (int)Symboru.feuille)
+                    ah = resultGame.win;
+                else
+                    ah = resultGame.again;
+            }
+            if (ah == resultGame.again)
+            {
+                _signPlayer = -1;
+                play = false;
+                Debug.Log("again");
+            }
+            else if (ah == resultGame.win)
+                Debug.Log("win");
+            else if (ah == resultGame.lose)
+                Debug.Log("lose");
+        }
+       
+    }
 }
---
+++ GDP12/Assets/Scripts/Sign.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sign : MonoBehaviour {
+
+    public enum IdSymbol
+    {
+        pierre,
+        feuille,
+        ciseaux
+    }
+
+    public IdSymbol _id;
+
+    Shifumi _game;
+
+	// Use this for initialization
+	void Start () {
+        _game = GetComponentInParent<Shifumi>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void    OnMouseDown()
+    {
+        _game._signPlayer = (int)_id;
+    }
+}
