47e38781df25e158706a97cf304e9816b9840502
Emeric Caramanna <emeric1.caramanna@epitech.eu>
2017-06-03 12:36:45 +0200

    Camera Following

---
+++ GDP12/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public Transform playerTransform;
+
+	Vector3 position = new Vector3();
+	
+	// Update is called once per frame
+	void Update () {
+		position = playerTransform.position;
+		position.z = transform.position.z;
+		transform.position = position;
+	}
+}
