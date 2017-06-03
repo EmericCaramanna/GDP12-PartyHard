c96c6975628f2671572097c6301b8825110bb636
Emeric Caramanna <emeric1.caramanna@epitech.eu>
2017-06-03 15:50:06 +0200

    minigames set up

---
+++ GDP12/Assets/Scripts/MiniGames.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGames : MonoBehaviour {
+
+	public enum GameName{
+		Dance,
+		Drink,
+		Fight,
+		Cop,
+		Shifumi,
+		BeerPong
+	}
+
+}
---
+++ GDP12/Assets/Scripts/MiniGamesManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGamesManager : MonoBehaviour {
+
+	public GameObject dance;
+	public GameObject drink;
+	public GameObject fight;
+	public GameObject cop;
+	public GameObject shifumi;
+	public GameObject beerPong;
+
+	public void LaunchGame(MiniGames.GameName gameName)
+	{
+		switch (gameName) {
+		case MiniGames.GameName.Dance : 
+			{
+				break;
+			}
+		case MiniGames.GameName.Drink :
+			{
+				break;
+			}
+		case MiniGames.GameName.Fight :
+			{
+				break;
+			}
+		case MiniGames.GameName.Cop :
+			{
+				break;
+			}
+		case MiniGames.GameName.Shifumi :
+			{
+				break;
+			}
+		case MiniGames.GameName.BeerPong :
+			{
+				break;
+			}
+		}
+	}
+}
