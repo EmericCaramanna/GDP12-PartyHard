a34395c003e57a02b192a7b21512650b94bdae0d
arnaud jomard <arnaud.jomard@epitech.eu>
2017-06-03 11:16:36 +0200

    ce que j'ai fait lol

---
+++ GDP12/Assets/Scripts/PnjBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PnjBehaviour : MonoBehaviour {
+
+    public GameObject playerReference;
+
+    public enum Role
+    {
+        randomGens,
+        videur,
+        barman
+    }
+
+    public Role     _id;
+    AudioSource     _audioSource;
+    BoxCollider2D   _boxCollider2D;
+    SpriteRenderer  _sprite;
+
+    Vector2         _posMouse;
+    // Function of Special Pnj
+    void    Videur()
+    {
+
+    }
+
+    void    Barman()
+    {
+
+    }
+
+    // Use this for initialization
+	void Start () {
+
+        _id = Role.randomGens;
+        if (_audioSource = GetComponent<AudioSource>())
+        {
+           //
+        }
+        if (_boxCollider2D = GetComponent<BoxCollider2D>())
+        {
+            //
+        }
+        if (_sprite = GetComponent<SpriteRenderer>())
+        {
+            //
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnMouseDown()
+    {
+        _audioSource.Play();
+    }
+}
