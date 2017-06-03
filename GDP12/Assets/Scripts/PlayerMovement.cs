70d8ff098fd6ec41edc7dc114bde8acc6d660470
Emeric Caramanna <emeric1.caramanna@epitech.eu>
2017-06-03 09:33:38 +0200

    Player Movement oriented according to mouse click

---
+++ GDP12/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 5f;
+	Rigidbody2D rigibody;
+
+	void Start()
+	{
+		rigibody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		ManageInput ();
+	}
+
+	void ManageInput()
+	{
+		if (Input.GetButton ("Fire1")) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			Vector2 diretion = new Vector2(ray.origin.x - transform.position.x, ray.origin.y - transform.position.y);
+			diretion.Normalize ();
+			if (rigibody) {
+				if (ray.origin != transform.position) {
+					rigibody.velocity = diretion * speed;
+					transform.up = rigibody.velocity;
+				} else
+					rigibody.velocity = Vector2.zero;
+			}
+		}
+		if (Input.GetButtonUp("Fire1")) {
+			if (rigibody) {
+				rigibody.velocity = Vector2.zero;
+			}
+		}
+	}
+
+}
