d0973673a4c22669cb0c988600bf35ddd7e33ff1
Emeric Caramanna <emeric1.caramanna@epitech.eu>
2017-06-03 21:35:44 +0200

    Beerpong

--- GDP12/Assets/Scripts/BeerPong.cs
+++ GDP12/Assets/Scripts/BeerPong.cs
@@ -1,16 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BeerPong : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Slider strength;
+	public Image fill;
+	public float variationRate;
+	float previousVariation;
+	public float sliderSpeed;
+	int sign;
+	RaycastHit2D hit;
+	int numberOfThrowLeft;
+	public ResourceManager resourceManager;
+
+	void OnEnable()
+	{
+		previousVariation = 0f;
+		sign = 1;
+		numberOfThrowLeft = 2;
+		previousVariation = 0f;
+		hit = new RaycastHit2D ();
+		strength.gameObject.SetActive(true);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (numberOfThrowLeft > 0) {
+			if (Time.time > variationRate + previousVariation) {
+				previousVariation = Time.time;
+				strength.value += sign * sliderSpeed / 100;
+				if (strength.value >= 1 || strength.value <= 0)
+					sign *= -1;
+			}
+			if (Input.GetMouseButtonDown (0)) {
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				hit = Physics2D.Raycast (ray.origin, ray.direction);
+				if (hit.transform != null && hit.transform.CompareTag ("Player")) {
+					if (strength.value > 0.5f && strength.value < 0.8f) {
+						resourceManager.AddAlcohol (10);
+						resourceManager.AddMoney (20);
+					} else {
+						resourceManager.AddAlcohol (40);
+						resourceManager.SubMoney (20);
+					}
+					numberOfThrowLeft--;
+					strength.value = 0f;
+				}
+			}
+			if (strength.value > 0.5f && strength.value < 0.8f) {
+				fill.color = Color.green;
+			} else
+				fill.color = Color.red;
+		} else {
+			strength.gameObject.SetActive(false);
+			gameObject.SetActive(false);
+		}
 	}
 }
--- GDP12/Assets/Scripts/ResourceManager.cs
+++ GDP12/Assets/Scripts/ResourceManager.cs
@@ -11,6 +11,11 @@ public class ResourceManager : MonoBehaviour {
 	public Text moneyText;
 	public Slider healthSlider;
 	public Slider alcoholSlider;
+	public float alcoholDecreaseRate;
+	public float healthDecreaseRate;
+	float previousAlcoholDecrease = 0f;
+	float previousHealthDecrease = 0f;
+	public float decreaseSpeed = 1;
 
 	public void AddAlcohol(float value)
 	{
@@ -45,6 +50,14 @@ public class ResourceManager : MonoBehaviour {
 	void Update()
 	{
 		moneyText.text = currentMoney + " $";
+		if (Time.time > alcoholDecreaseRate + previousAlcoholDecrease) {
+			previousAlcoholDecrease = Time.time;
+			SubAlcohol (decreaseSpeed);
+		}
+		if (Time.time > healthDecreaseRate + previousHealthDecrease) {
+			previousHealthDecrease = Time.time;
+			SubHealth (decreaseSpeed);
+		}
 		healthSlider.value = currentPercentHealth / 100;
 		alcoholSlider.value = currentPercentAlcohol / 100;
 	}
